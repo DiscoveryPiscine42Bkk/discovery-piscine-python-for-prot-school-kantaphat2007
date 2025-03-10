01e953d5eee3bcb968f80e1a273b0c7bb1e3e690
kantaphat2007 <40591@prot.ac.th>
2025-03-10 15:05:41 +0700

    Delete cell03/ex3.1/multiplication_table.py

--- cell03/ex3.1/multiplication_table.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter a number: "))  # รับค่าตัวเลขจากผู้ใช้
-num2 = 1  # เริ่มต้นตัวคูณที่ 1
-
-while num2 <= 12:  # ทำซ้ำตั้งแต่ 1 ถึง 12
-    num3 = num * num2  # คำนวณค่าผลคูณ
-    print(f"{num} x {num2} = {num3}")  # แสดงผลลัพธ์
-    num2 += 1

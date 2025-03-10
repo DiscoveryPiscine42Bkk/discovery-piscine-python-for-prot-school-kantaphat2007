25edf3d7a646b1dde223c2b1dd0202762fe14a33
kantaphat2007 <40591@prot.ac.th>
2025-03-11 00:02:48 +0700

    Add files via upload

---
+++ cell06/ex6.5/scope_that.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def add_one(n):
+    return n + 1  # Adds 1 to the passed parameter
+
+# Initialize a variable
+x = 5
+print(x)  # Display the initial value of x
+
+# Call the method but do not update the original variable
+add_one(x)
+
+print(x)  # Display the value of x again

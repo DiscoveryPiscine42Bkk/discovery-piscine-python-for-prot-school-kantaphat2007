dab784db403802824a64db84f5b680fc48bb5337
kantaphat2007 <40591@prot.ac.th>
2025-03-10 15:50:14 +0700

    Add files via upload

---
+++ cell04/ex4.1/age.py
@@ -0,0 +1,9 @@
+
+def main():
+    age = int(input("Enter your age: "))  
+    print(f"In 10 years, you will be {age + 10} years old.") 
+    print(f"In 20 years, you will be {age + 20} years old.")  
+    print(f"In 30 years, you will be {age + 30} years old.")  
+
+if __name__ == "__main__":
+    main()

68cdddff2dc76c408b61812815800984b40bcc91
kantaphat2007 <40591@prot.ac.th>
2025-03-10 19:16:11 +0700

    Add files via upload

---
+++ cell05/ex5.4/parameters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+# Calculate the number of parameters (excluding the script name itself)
+num_params = len(sys.argv) - 1
+
+# Print the result in the required format
+print(f"Number of parameters: {num_params}.")

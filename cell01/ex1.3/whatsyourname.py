cb3f347fe3dabf702dea7606fc03464289c19cb5
kantaphat2007 <40591@prot.ac.th>
2025-03-11 09:55:18 +0700

    Update whatsyourname.py

--- cell01/ex1.3/whatsyourname.py
+++ cell01/ex1.3/whatsyourname.py
@@ -1,3 +1,3 @@
-first_name = input("your first name : ")
-last_name = input("your last name : ")
-print(first_name, last_name)
+first_name = input("your first name: ")
+last_name = input("your last name: ")
+print("Well, pleased to meet you", first_name, last_name)
